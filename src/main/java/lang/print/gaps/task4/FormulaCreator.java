43efc14e540212e5fb504de0d37a0aac30278521
Eugene KAHADOC <eugene.bespalov@gmail.com>
2023-03-14 19:26:50 +0300

    very obscure requirements for the 3rd problem

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -2,5 +2,9 @@ package lang.print.gaps.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
+        int a=4;
+        int b = 12;
+        double res;
+        res = (9 * (a ^ 2) - 5 * b + 2 + a - 7) * ((a + b - 4 * a * b) / 2);
     }
 }
--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,5 +2,13 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        char x1 = 99;
+        char x2 = 104;
+        char x3 = 97;
+        char x4 = 114;
+        System.out.println(x1);
+        System.out.println(x2);
+        System.out.println(x3);
+        System.out.println(x4);
     }
 }
